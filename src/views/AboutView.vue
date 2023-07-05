b8b0f2ad5c00a98730bbd5c08754735069dcd886
Thabangx10 <Thabangx12@gmail.com>
2023-07-05 09:21:37 +0200

    updates on the 5th before the first tea

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -21,7 +21,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /* Body */
 body {
   text-align: center;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,16 @@
       <div class="row">
         <h2 class="display-2">
           Thabang Kganana
-          <img src="https://i.postimg.cc/mgRnzFL0/logo-1.jpg" alt="logo" class="img-thumbnail rounded-5" loading="lazy"
-            style="margin-top: 3%;" width="50%">
+          <img
+            src="https://i.postimg.cc/mgRnzFL0/logo-1.jpg"
+            alt="logo"
+            class="img-thumbnail rounded-5"
+            loading="lazy"
+            style="margin-top: 3%"
+            width="50%"
+          />
         </h2>
-        <h4 class="display-4"> I am<span>
-            a software developer
-          </span></h4>
+        <h4 class="display-4">I am<span> a software developer </span></h4>
       </div>
     </section>
   </div>
@@ -22,12 +26,23 @@
 export default {
   computed: {
     user() {
-      return this.$store.state.user
-    }
+      return this.$store.state.user;
+    },
   },
   mounted() {
-    return this.$store.dispatch('')
-  }
-
-}
+    return this.$store.dispatch("");
+  },
+};
 </script>
+
+<style scoped>
+div {
+  margin: auto;
+  text-align: center;
+  right: 0;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  color: black;
+}
+</style>
